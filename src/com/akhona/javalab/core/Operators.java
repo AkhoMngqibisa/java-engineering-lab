9b640bcca77035ee0b935155455199f786e05617
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-11-20 20:00:00 +0200

    feat(core-java): implement arithmetic operators example

--- src/com/akhona/javalab/core/Operators.java
+++ src/com/akhona/javalab/core/Operators.java
@@ -1,4 +1,19 @@
 package com.akhona.javalab.core;
 
 public class Operators {
+    public static void main(String[] args) {
+        implementArithmeticOperator();
+    }
+
+    private static void implementArithmeticOperator() {
+        int num1 = 7;
+        int num2 = 5;
+
+        int result = num1 % num2;
+        System.out.println(result);
+
+        System.out.println(num1++);
+        System.out.println(++num1);
+
+    }
 }
