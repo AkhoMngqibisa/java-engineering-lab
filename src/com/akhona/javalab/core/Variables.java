b67701360b393bcac38ebb3db1b91717b3740886
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-11-20 19:30:00 +0200

    feat(core-java): implement primitive data types examples

--- src/com/akhona/javalab/core/Variables.java
+++ src/com/akhona/javalab/core/Variables.java
@@ -1,10 +1,30 @@
 package com.akhona.javalab.core;
 
 public class Variables {
-    public static void main(String[] args) {
+    static void main(String[] args) {
+        declareVariables();
+    }
+
+    private static void declareVariables() {
         int num1 = 3;
         int num2 = 5;
 
         System.out.println(num1 + num2);
     }
+
+    private static void declareVariableDataTypes() {
+        int int1 = 9;
+        byte byte1 = 127;
+        short short1 = 558;
+        long long1 = 1234567890;
+
+        float float1 = 123.456f;
+        double double1 = 1234567890.123456789;
+
+        char char1 = 'a';
+
+        boolean bool1 = true;
+
+    }
+
 }
