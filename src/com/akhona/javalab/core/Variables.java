3a97e108003c675a4eeca09e722d40da11c7a8bf
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-11-20 19:15:00 +0200

    feat(core-java): implement variables examples

--- src/com/akhona/javalab/core/Variables.java
+++ src/com/akhona/javalab/core/Variables.java
@@ -1,4 +1,10 @@
 package com.akhona.javalab.core;
 
 public class Variables {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 5;
+
+        System.out.println(num1 + num2);
+    }
 }
